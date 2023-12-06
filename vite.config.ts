c5420f518b4ac110f287a4551c7fa7abaf07c774
dawei <dawei@163.com>
2023-12-06 22:12:55 +0800

    feat: 优化打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -103,28 +103,24 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
             // 打包大小超出 400kb 提示警告
             chunkSizeWarningLimit: 400,
             rollupOptions: {
-                // output: { experimentalMinChunkSize: 10240 },
                 // 打包入口文件 根目录下的 index.html
                 // 也就是项目从哪个文件开始打包
                 input: {
                     index: fileURLToPath(new URL('./index.html', import.meta.url))
                 },
-                external: ['moment', 'video.js', 'jspdf', 'xlsx', 'echart'],
-                plugins: [visualizer({ open: true }), globals],
+                plugins: [visualizer({ open: true })],
+                experimentalLogSideEffects: true,
+                treeshake: {
+                    preset: 'recommended'
+                    // propertyReadSideEffects: true
+                },
                 output: {
-                    // treeshake: {
-                    //     preset: 'recommended',
-                    //     manualPureFunctions: ['console.log']
-                    // }
+                    experimentalMinChunkSize: 20 * 1024,
                     manualChunks: (id: string) => {
-                        // html2canvas 只有极少数的页面使用了 所以要单独处理一下 第三方库分类打包
-                        if (id.includes('html2canvas')) {
-                            return 'html2canvas';
-                        }
                         if (id.includes('node_modules')) {
                             return 'vendor';
                         }
-                        return 'index';
+                        // return 'index';
                     }
                 }
 
