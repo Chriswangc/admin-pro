49439b24e65a3f797b258c3e50820afa0b8601cb
dawei <dawei@163.com>
2023-12-12 22:20:02 +0800

    feat: 通过elform组件的提示演示功能

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,6 +1,16 @@
 <template>
-    <div class="wrap-about">about</div>
+    <div class="wrap-about">
+        <ElForm ref="formRef"></ElForm>
+    </div>
 </template>
+<script setup lang="ts">
+// import { ref } from 'vue';
+import { useCompRef } from '@/hooks/useCompRef';
+import { ElForm } from 'element-plus';
+const formRef = useCompRef<typeof ElForm>();
+
+// formRef.value?.validate
+</script>
 <style lang="less" scoped>
 .wrap-about {
     // color: @color;
